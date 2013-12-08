f56adddbf29901b1bbd6aed53a3ec9ef6a9ba9bc
Sepp Silver <sepps1@niksula.hut.fi>
2013-12-08 17:28:55 +0100

    bigfixes. todo: splitting & controller

--- Prototype1/src/main/scala/eu/stratosphere/randomforest/RunRandomForest2.scala
+++ Prototype1/src/main/scala/eu/stratosphere/randomforest/RunRandomForest2.scala
@@ -26,7 +26,7 @@ import eu.stratosphere.scala.TextFile
 import eu.stratosphere.pact.common.`type`.base.PactInteger
 import eu.stratosphere.pact.common.`type`.base.PactString
 
-class RandomForest2 (trees : Array[Array[TreeNode2]], features: Array[Int]) extends PlanAssembler with PlanAssemblerDescription with Serializable {
+class RandomForest2 (trees : Array[Array[TreeNode2]]) extends PlanAssembler with PlanAssemblerDescription with Serializable {
   override def getDescription() = {
     "Usage: [inputPath] [outputPath] ([numSubtasks])"
   }
@@ -43,13 +43,10 @@ class RandomForest2 (trees : Array[Array[TreeNode2]], features: Array[Int]) exte
 		.flatMap { case (lineIndex, lineLabel, lineFeatures) => 
 		  trees
 		  	//get leaf nodes that need processing - with features that are available
-		  	.flatMap(tree => getNodesAndAvailableFeatures(tree))
+		  	.flatMap(tree => getNodesToSplit(tree))
 		  	//see if current data point valid for node
 		  	.filter { node => node.baggings.contains(lineIndex) }
-		  	.flatMap { node => 
-		  	  features
-		  	  	//only output features that are available for this branch
-		  	  	.filter(feature => node.features.contains(feature))
+		  	.flatMap { node =>  node.candidateFeatures
 		  	  	// get values for corresponding features and nodes
 		  	  	.map(feature => (node.treeId + "_" + node.nodeId, (feature, lineFeatures(feature), lineLabel, node.nodeId, lineIndex)))
 		  	}
@@ -76,10 +73,17 @@ class RandomForest2 (trees : Array[Array[TreeNode2]], features: Array[Int]) exte
   			//Get the best split
   			.maxBy( _._2 )
       	
+  		
       	if (bestSplit == null || bestSplit._2 < 10)
+      	{
+      		//TODO: change node to leaf node with a label???
       		(treeId, nodeId, null)
+      	}
       	else
+      	{
+      		//TODO: output new children node tuple
       		(treeId, nodeId, bestSplit)
+      	}
       }
     
     val sink = dataReduce.write(outputPath, CsvOutputFormat("\n", ","))
@@ -87,14 +91,19 @@ class RandomForest2 (trees : Array[Array[TreeNode2]], features: Array[Int]) exte
     new ScalaPlan(Seq(sink))
   }
   
-  
-  //tree id, feature id, value, label, bagging id
+  //tree id, feature id, value, label, data id / bagging id
   def getSplitInfo(featureId : Int, histogram : Array[(Int, Int, Int, Int, Int)]) = {
+    // { (P1, M1, L1, nodeId, dataId), ... (Pn, Mn, Ln, nodeId, dataId) }
+    
     //featureId, gain, new bagging table
-    (featureId, Random.nextInt(100))
+    
+    // 1) DELTA?
+    // 2) Number?
+    //(featureId, Random.nextInt(100), Random.nextInt(255))
+    (featureId, Random.nextInt(100), Random.nextInt(255))
   }
   
-  def getNodesAndAvailableFeatures(tree : Array[TreeNode2]) = {
+  def getNodesToSplit(tree : Array[TreeNode2]) = {
     var c: Array[TreeNode2] = Array();
     if (tree == null) c
     	
@@ -103,49 +112,25 @@ class RandomForest2 (trees : Array[Array[TreeNode2]], features: Array[Int]) exte
   }
 }
 
+///baggings: data for finding the best feature to split
+///features: all features left for this node
+///candidateFeatures: m features (subspace) from all features
+///feature: the selected feature index for this node
+///featureValue: the selected feature value (left child LESS than value, right side MORE than value)
 @serializable
-class TreeNode2(var baggings : Array[Int], var features : Set[Int], var candidateFeatures : Array[Int], var feature : Int, var treeId : Int, var nodeId : Int, var leaf : Boolean)
+class TreeNode2(
+    var baggings : Array[Int],
+    var features : Set[Int],
+    var candidateFeatures : Array[Int],
+    var feature : Int,
+    var featureValue : Int,
+    var treeId : Int,
+    var nodeId : Int,
+    var leaf : Boolean)
 {
-  def resetCandidateFeatures(){
-    
-  }
-}
-
-object RunRandomForest2 {
-  def main(args: Array[String]) {    
-    val treeCount = 10
-    val totalFeatureCount = 784
-    val dataCount = 100 //TODO: make this dynamic!!
-    //prepare trees and their bagging tables    
-    var trees = Array.fill(treeCount)(Array.fill(1)(new TreeNode2(Array.fill(dataCount)(Random.nextInt(dataCount-1)), (0 until totalFeatureCount).toSet, null, -1, -1, 1, false)))
-    // Assign id's to trees
-    for (treeId <- 0 to trees.length - 1)
-      trees(treeId)(0).treeId = treeId
-    
-    
-    var featureSubspaceCount = math.round(math.log(totalFeatureCount).toFloat + 1);
-    //prepare feature subspace
-    var featureSubspace = generateFeatureSubspace(featureSubspaceCount, totalFeatureCount)
-    
-	System.out.println("Generating '" + treeCount + "' trees, feature count: " + totalFeatureCount + ", with '" + featureSubspaceCount + "' feature subspace, data length: " + dataCount);
-    
-    val inputPath = new File("newdata.txt").toURI().toString()
-    println("Reading input from " + inputPath)
-    val outputPath = new File("C:\\Users\\Silver\\AppData\\Local\\Temp\\output").toURI().toString()
-    println("Writing output to " + outputPath)
-    
-    //var hasAnyNewTreeNodes = true;
-    //do
-    //{
-	    val plan = new RandomForest2(trees, featureSubspace).getPlan(inputPath, outputPath)
-	    Util.executePlan(plan)
-	    
-	    //var treeTup = parseOutput(job)
-	    //hasAnyNewTreeNodes = updateForests(trees,tree-tup)
-    //}while (hasAnyNewTreeNodes)
-
-    Util.deleteAllTempFiles()
-    System.exit(0)
+  def resetCandidateFeatures(featureSubspaceCount : Int, totalFeatureCount : Int){
+    if (candidateFeatures == null)
+    	candidateFeatures = generateFeatureSubspace(featureSubspaceCount, totalFeatureCount)
   }
   
   def generateFeatureSubspace(randomCount : Int, maxRandomNumber : Int) : Array[Int] = {
@@ -164,4 +149,55 @@ object RunRandomForest2 {
     }
     arr;
   }
+}
+
+object RunRandomForest2 {
+  def main(args: Array[String]) {    
+    val treeCount = 10
+    val totalFeatureCount = 784
+    val dataCount = 100 //TODO: make this dynamic!!
+    
+    var featureSubspaceCount = math.round(math.log(totalFeatureCount).toFloat + 1);
+    
+    //prepare trees and their bagging tables    
+    var trees = Array.fill(treeCount)(Array.fill(1)(new TreeNode2(
+        Array.fill(dataCount)(Random.nextInt(dataCount-1)), //baggings : Array[Int]
+        (0 until totalFeatureCount).toSet, //features : Set[Int]
+        null, //candidateFeatures : Array[Int]
+        -1, //feature
+        -1, //featureValue
+        -1, //treeId
+        1, //nodeId
+        false //leaf : Boolean
+        )))
+        
+    // Assign id's to trees
+    for (treeId <- 0 to trees.length - 1)
+      trees(treeId)(0).treeId = treeId
+      
+    // Assign id's to trees
+    for (treeId <- 0 to trees.length - 1)
+      trees(treeId)(0).resetCandidateFeatures(featureSubspaceCount, totalFeatureCount)
+    
+	System.out.println("Generating '" + treeCount + "' trees, feature count: " + totalFeatureCount + ", with '" + featureSubspaceCount + "' feature subspace, data length: " + dataCount);
+    
+    val inputPath = new File("newdata.txt").toURI().toString()
+    println("Reading input from " + inputPath)
+    val outputPath = new File("C:\\Users\\Silver\\AppData\\Local\\Temp\\output").toURI().toString()
+    println("Writing output to " + outputPath)
+    
+    //var hasAnyNewTreeNodes = true;
+    //do
+    //{
+	    val plan = new RandomForest2(trees).getPlan(inputPath, outputPath)
+	    Util.executePlan(plan)
+	    
+	    //var treeTup = parseOutput(job)
+	    //hasAnyNewTreeNodes = updateForests(trees,tree-tup)
+    //}while (hasAnyNewTreeNodes)
+
+    Util.deleteAllTempFiles()
+    System.exit(0)
+  }
+  
 }
